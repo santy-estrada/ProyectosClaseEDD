5c358d9a026fbfc4546d27f13c05ff2ba7540e1b
santy-estrada <santiago.estrada6@eia.edu.co>
2023-08-10 08:41:45 -0500

    SumaMaxima

--- EstructurasDeDatos/src/AnalisisAlgoritmos/SumaMaxima.java
+++ EstructurasDeDatos/src/AnalisisAlgoritmos/SumaMaxima.java
@@ -2,42 +2,45 @@ package AnalisisAlgoritmos;
 
 public class SumaMaxima {
 
-	public static int subsecuenciaSumaMaxima(int[]a) {
+	public static int[] subsecuenciaSumaMaxima(int[]a) {
 		
-		int sumaMax = 0;	//Valor máximo
+		int[] Vals = new int[3]; //Arrglo de valores: maxSuma, inicio, fin
 		int aux = 0;	//maximo auxiliar
-		int b = 0;	//beginning
-		int e = 0;	//end
 		boolean flag = true;	//Permite determinar el principio de la subsecuencia
  
         for (int i = 0; i < a.length; i++) {
         	
         	aux += a[i];	//acumula los valores
         
-            if (sumaMax < aux) {	//Si la sumaMax es menor que la suma auxiliar
+            if (Vals[0] < aux) {	//Si la sumaMax es menor que la suma auxiliar
             	if(flag) {			//Si va a empezar una subsecuencia
-            		b = i;			//Subsecuencia empieza en i
+            		Vals[1] = i;			//Subsecuencia empieza en i
             		flag = false;	//ya empezó la subsecuencia
+
             	}
-            	sumaMax = aux;		//sumaMax toma el valor del auxiliar
-            	e = i;				//Marca que acaba la subsecuencia en la pos i
+            	Vals[0] = aux;		//sumaMax toma el valor del auxiliar
+            	Vals[2] = i;				//Marca que acaba la subsecuencia en la pos i
+            	System.out.println("b");
+
             }
             
             if (aux < 0) {		//Si se acumuló un valor negativo
             	aux = 0;		//Reset
             	flag = true;	//Puede empezar una nueva secuencia 
+            	System.out.println("a");
             }
             
         }
-        System.out.println(b);
-        System.out.println(e);
 
-        return sumaMax;
+        return Vals;
 	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] a = {-1,-21,-3,4,-5,6};
-		System.out.println(subsecuenciaSumaMaxima(a));
+		int[] a = {1,-2,3,-4,5,-6,7};
+		for(int i: subsecuenciaSumaMaxima(a)) {
+			System.out.println(i);
+
+		}
 	}
 }
