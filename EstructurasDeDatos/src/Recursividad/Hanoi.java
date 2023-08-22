020383d71acb96db4584e79a3b98e320f4f8ea8b
santy-estrada <santiago.estrada6@eia.edu.co>
2023-08-22 17:56:28 -0500

    Hanoi

--- EstructurasDeDatos/src/Recursividad/Hanoi.java
+++ EstructurasDeDatos/src/Recursividad/Hanoi.java
@@ -12,10 +12,14 @@ public class Hanoi {
 	private static void hanoiR(int nDisco, char origen, char destino, char aux) {
 		if(nDisco == 1) {	//Caso base cuando se quiere organizar el último disco
 			System.out.println("Mover disco " + nDisco + " desde " + origen + " hasta " + destino);
+			//Este puede ocurrir cuando el objetivo es el destino o el auxiliar
 		}else {
 			hanoiR(nDisco-1, origen, aux, destino);// Mueve la pila inferior desde el origen al auxiliar
-			System.out.println("Mover disco " + nDisco + " desde " + origen + " hasta " + destino);
-			hanoiR(nDisco-1, aux, destino, origen); //Mueve 
+			//El primer objetivo es pasar todos los discos menos el último al auxiliar
+			System.out.println("Mover disco " + nDisco + " desde " + origen + " hasta " + destino);//Movimiento intermedio
+			//Moviemintos intermedios para llegar al objetivo
+			hanoiR(nDisco-1, aux, destino, origen); //Mueve  la pila desde auxiliar a C
+			//El segundo objetivo es pasar del auxiliar a la última torre
 			
 		}
 	}
