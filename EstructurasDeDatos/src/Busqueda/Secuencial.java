6464d7e45dcd37d30a323e6b3d53a1e7533aa632
santy-estrada <santiago.estrada6@eia.edu.co>
2023-09-05 15:32:52 -0500

    Tarea buscar el más cercano

--- EstructurasDeDatos/src/Busqueda/Secuencial.java
+++ EstructurasDeDatos/src/Busqueda/Secuencial.java
@@ -2,6 +2,7 @@ package Busqueda;
 
 public class Secuencial {
 	
+	//Búsqueda lineal
 	public static int busqueda(Comparable[] ps, Comparable p3) {
 		int i = 0;
 		while(i<ps.length && ps[i].compareTo(p3) != 0) i++;
@@ -9,6 +10,7 @@ public class Secuencial {
 		return(i == ps.length)? -1: i;
 	}
 	
+	//Búsqueda a partir de un índice
 	public static int searchIndex(Comparable[] a, Comparable e, int index) throws EIndexImposible{
 		if(index >= a.length) {
 			throw new EIndexImposible();
@@ -19,6 +21,61 @@ public class Secuencial {
 		return(index == a.length)? -1: index;
 	}
 	
+	//Búsqueda del más cercano a partir de un índice
+	public static int searchIndexClosest(int[] a, int e, int index) throws EIndexImposible{
+		if(index >= a.length) {
+			throw new EIndexImposible();
+		}
+		int auxI = index;
+		while(index < a.length && a[index] != e) {
+			if(Math.abs(a[index]-e) < Math.abs(a[auxI]-e)) {
+					auxI = index;
+			}
+			
+			if(Math.abs(a[index]-e) == Math.abs(a[auxI]-e) && a[index] < a[auxI]) {
+				auxI = index;
+			}
+			
+			index++;
+		}
+		return (index == a.length)? auxI: index;
+	}
+	
+	//Búsqueda del más cercano recursivamente
+	private static int searchClosestR(int a[], int e, int aux, int index, int i, int j) {
+		if(i > j) {
+			return aux;
+		}
+		
+		if(a[index] == e) {
+			return index;
+		}
+		
+		if(Math.abs(a[index]-e) < Math.abs(a[aux]-e)) {
+			aux = index;
+		}
+	
+		if(Math.abs(a[index]-e) == Math.abs(a[aux]-e) && a[index] < a[aux]) {
+			aux = index;
+		}
+		
+		//Tomar mitdad inferior
+		if(a[index] > e) {
+			j = index -1;
+			return searchClosestR(a, e, aux, (j-i)/2, i, j);
+		}else {
+			i = index +1;
+			return searchClosestR(a, e, aux, (j+i)/2, i, j);
+		}
+		
+	}
+	
+	public static int searchClosest(int a[], int e) {
+		int j = a.length-1;
+		return searchClosestR(a, e, 0, j/2,0,j);
+	}
+	
+	//Búsqueda binaria iterativa
 	public static int busquedaBinariaI(Comparable[] a, Comparable e) {
 		int i = 0;
 		int j = a.length -1;
@@ -39,6 +96,7 @@ public class Secuencial {
 		return result;
 	}
 
+	//Búsqueda binaria recursiva
 	private static int busquedaBinariaR(Comparable[]a, Comparable e, int index, int i, int j) {
 		if(a[index].compareTo(e) == 0) {
 			return index;
@@ -61,12 +119,35 @@ public class Secuencial {
 		return busquedaBinariaR(a, e,j/2,0,j);
 	}
 	
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
 		Integer arr[] = {0,1,2,3};
+
+		//Búsqueda binaria
 		System.out.println(busquedaBinariaI(arr, 5));
 		System.out.println(binarySearch(arr, 5));
+		
+		//Búsqueda con índice
+		try {
+			System.out.println(searchIndex(arr, 0,1));
+		} catch (EIndexImposible e) {
+			// TODO Auto-generated catch block
+			System.out.println(e.getMessage());
+		}
+		
+		//Búsqueda del más cercano
+		int a[] = {1,6,9,8,0,3,4};
+		try {
+			System.out.println(searchIndexClosest(a, 10, 0));
+		} catch (EIndexImposible e) {
+			// TODO Auto-generated catch block
+			System.out.println(e.getMessage());
+		}
+		int b[] = {0,1,3,4,6,8,9, 400};
+		System.out.println(searchClosest(b, 400));
+	
 	}
 
 }
